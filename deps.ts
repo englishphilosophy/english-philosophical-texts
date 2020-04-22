cc80d57c315a4d719c607276a0740fe3151a1970
Amyas Merivale <merivale@gmail.com>
2020-04-22 17:27:38 +0200

    various edits

---
+++ deps.ts
@@ -0,0 +1,19 @@
+export {
+  green,
+  red
+} from 'https://deno.land/std/fmt/colors.ts'
+
+export {
+  ensureDirSync,
+  readJsonSync,
+  readFileStrSync,
+  writeFileStrSync
+} from 'https://deno.land/std/fs/mod.ts'
+
+export {
+  dirname
+} from 'https://deno.land/std/path/mod.ts'
+
+export {
+  parse as parseYaml
+} from 'https://deno.land/std/encoding/yaml/parse.ts'
