7d1c14309e5ca4a459daa5a489a2bfe51372fb44
Amyas Merivale <merivale@gmail.com>
2020-05-22 21:13:49 +0200

    fixed Hume.THN parents; set remote dependencies

--- deps.ts
+++ deps.ts
@@ -4,24 +4,24 @@ export {
   readJsonSync,
   readFileStrSync,
   writeFileStrSync
-} from 'https://deno.land/std@v0.52.0/fs/mod.ts'
+} from 'https://deno.land/std@v0.42.0/fs/mod.ts'
 
 export {
   dirname
-} from 'https://deno.land/std@v0.52.0/path/mod.ts'
+} from 'https://deno.land/std@v0.42.0/path/mod.ts'
 
 export {
   parse as parseArgs
-} from 'https://deno.land/std@v0.52.0/flags/mod.ts'
+} from 'https://deno.land/std@v0.42.0/flags/mod.ts'
 
 export {
   parse as parseYaml
-} from 'https://deno.land/std@v0.52.0/encoding/yaml.ts'
+} from 'https://deno.land/std@v0.42.0/encoding/yaml.ts'
 
 export {
   green,
   red
-} from 'https://deno.land/std@v0.52.0/fmt/colors.ts'
+} from 'https://deno.land/std@v0.42.0/fmt/colors.ts'
 
-import markit from '../markit/mod.ts'
+import markit from 'https://raw.githubusercontent.com/englishphilosophy/markit/0.1.0/mod.ts'
 export { markit }
