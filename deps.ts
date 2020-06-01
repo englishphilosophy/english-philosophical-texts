bb38f3cc3edb663bba054fa2249f23f3933ea2c7
Amyas Merivale <merivale@gmail.com>
2020-06-01 10:13:48 +0200

    updated deno dependencies

--- deps.ts
+++ deps.ts
@@ -4,24 +4,24 @@ export {
   readJsonSync,
   readFileStrSync,
   writeFileStrSync
-} from 'https://deno.land/std@v0.42.0/fs/mod.ts'
+} from 'https://deno.land/std@v0.54.0/fs/mod.ts'
 
 export {
   dirname
-} from 'https://deno.land/std@v0.42.0/path/mod.ts'
+} from 'https://deno.land/std@v0.54.0/path/mod.ts'
 
 export {
   parse as parseArgs
-} from 'https://deno.land/std@v0.42.0/flags/mod.ts'
+} from 'https://deno.land/std@v0.54.0/flags/mod.ts'
 
 export {
   parse as parseYaml
-} from 'https://deno.land/std@v0.42.0/encoding/yaml.ts'
+} from 'https://deno.land/std@v0.54.0/encoding/yaml.ts'
 
 export {
   green,
   red
-} from 'https://deno.land/std@v0.42.0/fmt/colors.ts'
+} from 'https://deno.land/std@v0.54.0/fmt/colors.ts'
 
 import markit from 'https://raw.githubusercontent.com/englishphilosophy/markit/v0.1.0/mod.ts'
 export { markit }
