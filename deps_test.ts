95b4eddbce5dea7f9a0cd85927788867f26e5297
Amyas Merivale <merivale@gmail.com>
2020-06-13 16:17:42 +0200

    updated dependencies

--- deps_test.ts
+++ deps_test.ts
@@ -1,16 +1,16 @@
 export {
   assert,
   assertEquals
-} from 'https://deno.land/std@v0.54.0/testing/asserts.ts'
+} from 'https://deno.land/std@v0.57.0/testing/asserts.ts'
 
 export {
   dirname
-} from 'https://deno.land/std@v0.54.0/path/mod.ts'
+} from 'https://deno.land/std@v0.57.0/path/mod.ts'
 
 export {
   existsSync,
   readFileStrSync
-} from 'https://deno.land/std@v0.54.0/fs/mod.ts'
+} from 'https://deno.land/std@v0.57.0/fs/mod.ts'
 
-import markit from 'https://raw.githubusercontent.com/englishphilosophy/markit/v0.1.0/mod.ts'
+import markit from 'https://raw.githubusercontent.com/englishphilosophy/markit/v0.2.0/mod.ts'
 export { markit }
