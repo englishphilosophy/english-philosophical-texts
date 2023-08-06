8c66bef63bc97a889a760ae69e42928b4bec07f9
Amyas Merivale <merivale@gmail.com>
2023-08-06 11:09:39 +0100

    Edit public interface

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
+import "https://deno.land/std@0.194.0/dotenv/load.ts";
 import type {
   Lexicon,
   FlatLexicon,
@@ -7,61 +8,30 @@ import type { Author, Text } from "./src/types/library.ts";
 import type { Analysis, Lemma } from "./src/types/analysis.ts";
 import type { QueryParams, SearchResult } from "./src/types/search.ts";
 
-export type { Analysis, Lemma, LemmatizeResult } from "./src/types/analysis.ts";
-
-export type {
-  Lexicon,
-  FlatLexicon,
-  ReducedLexicon,
-} from "./src/types/lexicon.ts";
-
-export type {
-  Data,
-  Author,
-  Block,
-  SourceText,
-  Text,
-  TextStub,
-} from "./src/types/library.ts";
-
-export { isAuthor, isText, isTextStub, isBlock } from "./src/types/library.ts";
-
-export type {
-  Operator,
-  Query,
-  QueryParams,
-  SearchOptions,
-  SearchResult,
-} from "./src/types/search.ts";
-
-let base = "http://localhost:3001";
-
-export const setBase = (newBase: string): void => {
-  base = newBase;
-};
+const baseUrl = "https://ept.deno.dev";
 
 export const lexicon = async (): Promise<Lexicon> => {
-  const response = await fetch(`${base}/lexicon`);
+  const response = await fetch(`${baseUrl}/lexicon`);
   return await response.json();
 };
 
 export const flatLexicon = async (): Promise<FlatLexicon> => {
-  const response = await fetch(`${base}/lexicon-flat`);
+  const response = await fetch(`${baseUrl}/lexicon-flat`);
   return await response.json();
 };
 
 export const reducedLexicon = async (): Promise<ReducedLexicon> => {
-  const response = await fetch(`${base}/lexicon-reduced`);
+  const response = await fetch(`${baseUrl}/lexicon-reduced`);
   return await response.json();
 };
 
 export const authors = async (): Promise<Author[]> => {
-  const response = await fetch(`${base}/authors`);
+  const response = await fetch(`${baseUrl}/authors`);
   return await response.json();
 };
 
 export const author = async (id: string): Promise<Author | undefined> => {
-  const response = await fetch(`${base}/authors`);
+  const response = await fetch(`${baseUrl}/authors`);
   const authors = (await response.json()) as Author[];
   return authors.find((author) => author.id.toLowerCase() === id.toLowerCase());
 };
@@ -72,7 +42,7 @@ export const text = async (
 ): Promise<Text | undefined> => {
   try {
     const sanitizedId = id.toLowerCase().replaceAll(".", "/");
-    const response = await fetch(`${base}/${type}/${sanitizedId}`);
+    const response = await fetch(`${baseUrl}/${type}/${sanitizedId}`);
     const result = await response.json();
     return result.error ? undefined : result;
   } catch {
@@ -83,7 +53,7 @@ export const text = async (
 export const analysis = async (id: string): Promise<Analysis | undefined> => {
   try {
     const sanitizedId = id.toLowerCase().replaceAll(".", "/");
-    const response = await fetch(`${base}/analysis/${sanitizedId}`);
+    const response = await fetch(`${baseUrl}/analysis/${sanitizedId}`);
     const result = await response.json();
     return result.error ? undefined : result;
   } catch {
@@ -94,7 +64,7 @@ export const analysis = async (id: string): Promise<Analysis | undefined> => {
 export const lemmas = async (id: string): Promise<Lemma[] | undefined> => {
   try {
     const sanitizedId = id.toLowerCase().replaceAll(".", "/");
-    const response = await fetch(`${base}/lemmas/${sanitizedId}`);
+    const response = await fetch(`${baseUrl}/lemmas/${sanitizedId}`);
     const result = await response.json();
     return result.error ? undefined : result;
   } catch {
@@ -105,7 +75,7 @@ export const lemmas = async (id: string): Promise<Lemma[] | undefined> => {
 export const search = async (
   queryParams: QueryParams
 ): Promise<SearchResult[]> => {
-  const response = await fetch(`${base}/search`, {
+  const response = await fetch(`${baseUrl}/search`, {
     method: "POST",
     body: JSON.stringify(queryParams),
   });
